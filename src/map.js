9b7ee0ec6ed325075c3824b300abf70d4c3e9cb0
연훈 배 <sbaru1997@naver.com>
2021-06-03 13:45:38 +0900

    Update

---
+++ src/map.js
@@ -0,0 +1,9 @@
+function init(){
+    var mapContainer = document.getElementById('map'), 
+    mapOption = { 
+        center: new kakao.maps.LatLng(36.33929480377198, 127.39343696623958),
+        level: 7
+    };
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
+};
+init();
