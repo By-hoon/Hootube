ac694d0e7079e9b2301ed9b5b993229e5a2f40a7
연훈 배 <sbaru1997@naver.com>
2021-06-17 20:25:57 +0900

    Add make marker

--- src/controllers/mapController.js
+++ src/controllers/mapController.js
@@ -1,11 +1,10 @@
 import Story from "../models/Story";
 
-export const mapPrint =  (req, res) => {
-    return res.render("map", { pageTitle: "map" });
+export const mapPrint = async (req, res) => {
+    const stories = await Story.find({}).sort({ createdAt: "desc" });
+    return res.render("marker", { pageTitle: "Map" , stories});
 };
 
-export const markerAdd =  async (req, res) => {
-    const stories = await Story.find({}).sort({ createdAt: "desc" });
-    return res.render("marker", { pageTitle: "Marker Add" , stories});
+export const markerAdd =  (req, res) => {
 };
 
